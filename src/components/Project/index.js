ea8c57da3ef0abc0719ff4ba5add5353f826af40
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-04-25 15:53:15 -0400

    rough draft of portfolio page finished

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,36 +4,57 @@ function Project() {
 
     const [projects] = useState([
         {
-            name: 'Project 1'
+            name: 'Budget Tracker',
+            deployedUrl: 'https://warm-oasis-55894.herokuapp.com/',
+            githubUrl: 'https://github.com/perfect-perfect/budget-tracker'
         },
         {
-            name: 'Project 2'
+            name: 'Tech Blog',
+            deployedUrl: 'https://damp-cliffs-35712.herokuapp.com/',
+            githubUrl: 'https://github.com/perfect-perfect/tech-blog'
         },
         {
-            name: 'Project 3'
+            name: 'Note Taker',
+            deployedUrl: 'https://gentle-falls-04879.herokuapp.com/notes',
+            githubUrl: 'https://github.com/perfect-perfect/note-taker'
         },
         {
-            name: 'Project 4'
+            name: 'Comics and Cocktails',
+            deployedUrl: 'https://perfect-perfect.github.io/comics-and-cocktails/',
+            githubUrl: 'https://github.com/perfect-perfect/comics-and-cocktails'
         },
         {
-            name: 'Project 5'
+            name: 'Password Generator',
+            deployedUrl: 'https://perfect-perfect.github.io/password-generator/',
+            githubUrl: 'https://github.com/perfect-perfect/password-generator'
         },
         {
-            name: 'Project 6'
+            name: 'HTML Portfolio',
+            deployedUrl: 'https://perfect-perfect.github.io/portfolio/',
+            githubUrl: 'https://github.com/perfect-perfect/portfolio'
         }
     ])
 
     return (
         <div>
-            <div className='flex-row'>
+            <div >
                 {projects.map((image, i) => (
-                    <img
-                        src={require(`../../assets/portfolio/${i}.png`)}
-                        alt={image.name}
-                        className='img-thumbnail mx-1'
-                        // onClick={() =>}
-                        key={image.name}
-                    />    
+                    <section className="mx-5 my-5">
+                        <h1>{image.name}</h1>
+                        <img
+                            // src={require(`../../assets/portfolio/${i}.png`)}
+                            src={require(`../../assets/portfolio/${i}.png`)}
+                            alt={image.name}
+                            className='img-thumbnail mx-1'
+                            // onClick={() =>}
+                            key={image.name}
+                        /> 
+                        <section className="flex-row mx-5">
+                            <a href={image.deployedUrl}>Deployed Application</a>
+                            |
+                            <a href={image.githubUrl}>GitHub Repo</a>
+                        </section>
+                    </section>
                 ))}
 
             </div>
