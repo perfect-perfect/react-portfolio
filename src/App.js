7a3bd4a8513acd2e9f5a2f5d9178192a67bae8a1
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-04-25 18:50:07 -0400

    Change document.title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-// import './App.css';
 import React, { useState } from 'react';
 import Header from './components/Header';
-// import Gallery from './components/Gallery';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
 import Resume from './components/Resume';
@@ -10,30 +8,15 @@ import Footer from './components/Footer';
 
 function App() {
 
-
-
-	const [categories] = useState([
-        {
-            name: "portfolio",
-        },
-        {
-            name: "resume",
-        }
-    ]);
-
 	const [aboutMeSelected, setAboutMeSelected] = useState(true);
 	const [portfolioSelected, setPortfolioSelected] = useState(false);
 	const [resumeSelected, setResumeSelected] = useState(false);
 	const [contactSelected, setContactSelected] = useState(false)
 
-	const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
 	return (
 		<div>
 			<Header
-				categories={categories}
-				setCurrentCategory={setCurrentCategory}
-
 				aboutMeSelected={aboutMeSelected}
 				setAboutMeSelected={setAboutMeSelected}
 
@@ -48,9 +31,8 @@ function App() {
 			></Header>
 			<main>
 				{portfolioSelected && 
-					<Portfolio currentCategory={currentCategory}></Portfolio>
+					<Portfolio></Portfolio>
 				}
-				{/* <Portfolio currentCategory={currentCategory}></Portfolio> */}
 				{aboutMeSelected &&
 					<About></About>
 				}
